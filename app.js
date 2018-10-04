fd5a9836a53860b25349a3fb2234ed8877e263b4
M. Neuber <mneuber55@gmail.com>
2018-10-03 22:51:06 -0400

    final

--- app.js
+++ app.js
@@ -14,11 +14,16 @@ function callGifs() {
             gifDiv.attr("class", "added-gif-div");
             $(gifDiv).append("<p> Rating: "+response.data[i].rating.toUpperCase()+"</p>");
             var gif = $("<img>");
-            gif.attr("src", response.data[i].images.fixed_height.url);
+            gif.attr("fixed-data", response.data[i].images.fixed_height_still.url)
+            gif.attr("moving-data", response.data[i].images.fixed_height.url)
+            gif.attr("src", response.data[i].images.fixed_height_still.url);
             gif.attr("class", "added-gif");
+            gif.attr("status", "fixed");
             $(gifDiv).append(gif);
             $("#gif-space").append(gifDiv);
         }
+        $("img").off("click");
+        moveGifs();
     });
 };
 
@@ -35,7 +40,6 @@ function populateButtons() {
     //Start button click event listener
     $("button").on("click", function() {
         event.preventDefault();
-        console.log($(this).attr("animal").trim());
         searchTerm = $(this).attr("animal").trim();
         callGifs();
     });
@@ -47,6 +51,22 @@ function newButton() {
         var newTopic = $("#search-box").val().trim();
         topics.push(newTopic);
         populateButtons();
+};
+
+//Function for making gif move or stop moving on click
+function moveGifs() {
+    $("img").on("click", function() {
+        event.preventDefault();
+        console.log(this);
+        if ($(this).attr("status") == "fixed") {
+            $(this).attr("src", $(this).attr("moving-data"));
+            $(this).attr("status", "moving");
+        }
+        else if ($(this).attr("status") == "moving") {
+            $(this).attr("src", $(this).attr("fixed-data"));
+            $(this).attr("status", "fixed");
+        }
+    });
 };
 
 //Document ready to stage all functions
